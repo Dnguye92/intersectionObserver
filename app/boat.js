d3e42609a2e820d7d6b14440865e8666f64b6aa2
Dylan Nguyen <Dnguye92@yahoo.com>
2017-09-26 16:49:42 -0700

    managed to get intersection Observer working. Maybe it's not compatible with bootstrap?

--- app/boat.js
+++
@@ -1,9 +0,0 @@
-const Boatit = require('boatit');
-
-const boatit = new Boatit();
-
-process.stdin
-    .pipe(boatit)
-    .pipe(process.stdout);
-
-console.log('boat boat boat boat boat boat boat');
